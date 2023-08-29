3853ac8ed5ba700f2a8380095195cfe6a5552a3d
MJavad Alavi <smja1376@gmail.com>
2023-08-29 12:52:46 +0330

    Update 2023_08_23_064055_create_callbacks_table.php

--- src/database/migrations/2023_08_23_064055_create_callbacks_table.php
+++ src/database/migrations/2023_08_23_064055_create_callbacks_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('callbacks', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")->constrained();
             $table->json("data");
             $table->timestamps();
         });
